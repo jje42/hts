d6ded477eea10f22c45e6e2c127d2ff48fa8c697
Jonathan Ellis <jonathan.j.ellis@gmail.com>
2021-11-05 09:49:22 +1000

    Added variant tests

---
+++ vcf/variant_test.go
@@ -0,0 +1,54 @@
+package vcf
+
+import "testing"
+
+func TestVariant_Type(t *testing.T) {
+	type fields struct {
+		Chrom     string
+		Pos       int
+		ID        string
+		Ref       string
+		Alt       []string
+		Qual      string
+		Filter    []string
+		Info      map[string]string
+		Format    []string
+		genotypes []Genotype
+		header    *Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   Type
+	}{
+		{"t1", fields{Ref: "A", Alt: []string{"C"}}, SNP},
+		{"t2", fields{Ref: "ATGC", Alt: []string{"CGTA"}}, MNP},
+		{"t3", fields{Ref: "A", Alt: []string{"ATGC"}}, INDEL},
+		{"t4", fields{Ref: "ATGC", Alt: []string{"A"}}, INDEL},
+		{"t5", fields{Ref: "A", Alt: []string{"C", "G"}}, SNP},
+		{"t6", fields{Ref: "ATGC", Alt: []string{"AAAA", "TTTT"}}, MNP},
+		{"t7", fields{Ref: "ATGC", Alt: []string{"AT", "AGTGTA"}}, INDEL},
+		{"t8", fields{Ref: "A", Alt: []string{"C", "ATGC"}}, MIXED},
+		{"t8", fields{Ref: "A", Alt: []string{}}, NO_VARIATION},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := Variant{
+				Chrom:     tt.fields.Chrom,
+				Pos:       tt.fields.Pos,
+				ID:        tt.fields.ID,
+				Ref:       tt.fields.Ref,
+				Alt:       tt.fields.Alt,
+				Qual:      tt.fields.Qual,
+				Filter:    tt.fields.Filter,
+				Info:      tt.fields.Info,
+				Format:    tt.fields.Format,
+				genotypes: tt.fields.genotypes,
+				header:    tt.fields.header,
+			}
+			if got := v.Type(); got != tt.want {
+				t.Errorf("Variant.Type() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
